f71754ca748a84dfa3868985264cbe0c05bfee9e
Francesco Stablum <stablum@gmail.com>
2015-11-14 17:56:10 +0100

    refactored by adding Tome

--- count.py
+++ count.py
@@ -1,44 +1,39 @@
 import pandas as pd
 import argparse
 import os
-import gzip
 import glob
 
 # local imports
 import triple
 
-def process(filename):
-    handle = gzip.open(filename, 'rb')
-    df = pd.DataFrame.from_csv(
-        handle,
-        sep='\t',
-        #names=triple.Triple.members(),
-        header=None,
-        index_col=None
-    )
-    counted = df.groupby([0,1,2]).sum().reset_index()
-    return counted
-    
 def main():
     ap = argparse.ArgumentParser("Counting the triples")
     ap.add_argument("-d", nargs=1, help="directory containing the preprocessed data")
     ap.add_argument("-c", nargs=1, help="directory containing the counted triples")
+
+    ap.add_argument("-m", nargs=1, help="members to group and collapse in the sum, comma separated")
     args = ap.parse_args()
     d = args.d[0]
     c = args.c[0]
+    try:
+        m = args.m[0].split(',')
+    except:
+        m = ['v','s','o']
     for curr in [c,d]:
         if not os.path.exists(curr):
             raise Exception("directory %s does not exist"%curr)
     
     for filename in glob.glob(os.path.join(d,"*")):
         print "processing file %s.."%filename
-        df = process(filename)
-        dst_filename = os.path.join(c,os.path.basename(filename))
-        print "writing to %s.."%dst_filename
-        handle = gzip.open(dst_filename, "wb")
-        for i,row in df.iterrows():
-            t = triple.Triple(*row)
-            handle.write(t.totabs()+'\n')
+        tome_in = triple.Tome(filename)
+        filename_out = os.path.join(c,os.path.basename(filename))
+        print "writing to %s.."%filename_out
+        tome_out = triple.Tome(filename_out)
+        
+        writer = tome_out.writer()
+        for tr in tome_in.group_sum(m):
+            writer(tr)
+
 if __name__=="__main__":
     main()
 
--- triple.py
+++ triple.py
@@ -1,3 +1,7 @@
+import gzip
+import pandas as pd
+import os
+
 class Triple(dict):
     
     def __init__(self,*args):
@@ -22,4 +26,63 @@ class Triple(dict):
 
     def __repr__(self):
         return 'Triple('+','.join(map(str,self.tolist()))+')'
+
+class Tome(object):
+    
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def _unbox_as_df(self):
+
+        if not os.path.isfile(self.filename):
+            raise Exception("file %s does not exist"%self.filename)
+        handle = gzip.open(self.filename, 'rb')
+        df = pd.DataFrame.from_csv(
+            handle,
+            sep='\t',
+            #names=Triple.members(),
+            header=None,
+            index_col=None
+        )
+        return df
+    
+    def _group(self, fields=None):
+
+        df = self._unbox_as_df()
+
+        if fields is None:
+            fields = Triple.members()
+        
+        field_idx = map(Triple.members().index,fields)
+        ret = df.groupby(field_idx)
+        return ret
+    
+    def _group_sum_df(self,fields):
+        tmp = self._group(fields)
+        ret = tmp.sum().reset_index()
+        return ret
+    
+    def group_sum(self, fields):
+        tmp = self._group_sum_df(fields)
+        for i,row in tmp.iterrows():
+            yield Triple(*row)
+    
+    def writer(self):
+        handle = gzip.open(self.filename, "wb")
+
+        def _fn(payload):
+            if type(payload) is Triple:
+                buf = payload.totabs()+'\n'
+            else:
+                buf = payload
+            handle.write(buf)
+            handle.flush()
+        
+        return _fn
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return 'Tome("%s")'%self.filename
 
