ce7904db3b8036cd954ce49934fdfdb85af57d75
kofolaloka <e.hartlova@gmail.com>
2015-11-27 12:16:00 +0100

    fixed EM

--- frame_induction.py
+++ frame_induction.py
@@ -1,5 +1,5 @@
 # a couple of modules that might come in handy
-from itertools import islice, izip
+from itertools import islice, izip, chain
 import io, sys
 import argparse
 import time
@@ -7,6 +7,7 @@ import datetime
 from collections import Counter, defaultdict
 from multiprocessing import Pool
 import math
+import numpy as np
 
 def main():
 	parser = argparse.ArgumentParser(description='Find alignments of bilingual corpus using EM training')
@@ -23,8 +24,13 @@ def main():
 	data = readData(args['input'])
 	N = len(data)
 
+	global vocabulary
+	global V
+	vocabulary = extractVocabulary(data)
+	V = len(vocabulary)
 	if args['model'] == '0-Rooth':
-		results = emTraining(data)
+		emTraining()
+
 
 def readData(dataFile):
 	print 'Reading data...'
@@ -36,70 +42,95 @@ def readData(dataFile):
 			#break
 	return data
 
+
+def extractVocabulary(data):
+	vocabulary = []
+	for triple, count in data:
+		vocabulary += triple
+	return list(set(vocabulary))
+
+
 def estimatePosterior(phi, theta):
-	pTable = [[[float(1)/frames]*frames]*3 for i in xrange(N)]
+	pTable = [np.zeros(frames) for w in xrange(V)]
 	for i in xrange(N):
-		for w in xrange(3):
+		for a in xrange(3):
+			w = vocabulary.index(data[i][0][a])
 			for f in xrange(frames):
-				pTable[i][w][f] = theta[i][f]*phi[f][w]
+				pTable[w][f] = theta[f]*phi[f][a][w]
+	# normalize
+	for w in xrange(V):	
+		num = sum(pTable[w])
+		pTable[w] = [pTable[w][f]/num for f in xrange(frames)]
 	return pTable
 
+
 def maximizePhi(pTable):
-	phi = [range(3) for f in xrange(frames)]
+	phi = [[np.zeros(V) for a in xrange(3)] for f in xrange(frames)]
 	for f in xrange(frames):
-		for w in xrange(3):
-			phi[f][w] = sum(data[i][1]*pTable[i][w][f] for i in xrange(N))
-		num = sum(phi[f])
-		phi[f] = [phi[f][w]/num for w in xrange(3)]
+		for a in xrange(3):
+			for w in xrange(V):
+				for i in xrange(N):
+					if data[i][0][a] == vocabulary[w]:
+						phi[f][a][w] += data[i][1]*pTable[w][f]
+			# normalize
+			num = sum(phi[f][a])
+			phi[f][a] = [phi[f][a][w]/num for w in xrange(V)]
 	return phi
 
+
 def maximizeTheta(pTable):
-	theta = [range(frames) for i in xrange(N)]
-	for i in xrange(N):
-		for f in xrange(frames):
-			theta[i][f] = sum(data[i][1]*sum(pTable[i][w][f] for w in xrange(3)) for i in xrange(N))
-		num = sum(theta[i])
-		theta[i] = [theta[i][f]/num for f in xrange(frames)]
+	theta = np.zeros(frames)
+	for f in xrange(frames):
+		theta[f] = sum(data[i][1]*sum(pTable[vocabulary.index(w)][f] for w in data[i][0]) for i in xrange(N))
+		# normalize
+	num = sum(theta)
+	theta = [theta[f]/num for f in xrange(frames)]
 	return theta
+
 
 def emLikelihood(pTable, phi, theta):
 	ll = 0
 	for i in xrange(N):
-		for w in xrange(3):
+		count = data[i][1]
+		for a in xrange(3):
 			for f in xrange(frames):
-				ll += data[i][1]*pTable[i][w][f]*math.log(theta[i][f]*phi[f][w])
+				w = vocabulary.index(data[i][0][a])
+				ll += count*pTable[w][f]*math.log(theta[f]*phi[f][a][w])
 	return ll
 
-def emTraining(iters=10):
+
+def emTraining(iterations=10):
 	print 'Beginning EM training...'
 	globalStart = time.time()
 
 	print 'Initializing...'
-	# NxWxF
-	pTable = [[[float(1)/frames for f in xrange(frames)] for w in xrange(3)] for i in xrange(N)]
+	# random initialization
+	phi = [[np.random.dirichlet(np.ones(V),size=1)[0] for wa in xrange(3)] for f in xrange(frames)]
+	# uniform initialization
+	theta = [float(1)/frames for f in xrange(frames)]
 
-	#print pTable[0]
-	for i in xrange(1,11):
+	for i in xrange(1,(iterations+1)):
 		print '\tIteration', str(i)
 		start = time.time()
 
+		pTable = estimatePosterior(phi, theta) 
 		phi = maximizePhi(pTable)
 		theta = maximizeTheta(pTable)
-		pTable = estimatePosterior(phi, theta) 
 
 		print '\t\tIteration completed in', getDuration(start, time.time())
 		print '\t\tLog Likehood:', str(emLikelihood(pTable, phi, theta))
 	print 'EM training completed in', getDuration(globalStart, time.time())
-	
-	#print pTable[0]
+
 
 def lda(prior=False):
 	print 'Beginning LDA...'
 	globalStart = time.time()
 	print 'LDA completed in', getDuration(globalStart, time.time())
 
+
 def getDuration(start, stop):
     return str(datetime.timedelta(seconds=(stop-start)))
+
 
 if __name__ == '__main__':
     main()
